8ba90c5f185532cbbd7b21c62e89ea139079599c
mozahzah <mozahzah@gmail.com>
2025-02-14 17:16:11 -0600

    Updated imgui thirdparty lib, imported IMGUI CMake to IECore

--- Source/IERenderer.h
+++ Source/IERenderer.h
@@ -15,8 +15,8 @@
 
 #include "GLFW/glfw3.h"
 #include "GLFW/glfw3native.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_vulkan.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_vulkan.h"
 
 #include "IEUtils.h"
 
